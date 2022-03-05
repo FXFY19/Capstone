f829b0da5b5d27f4618a2056ad27c39d8d935c3f
FXFY19 <99360332+FXFY19@users.noreply.github.com>
2022-03-04 19:34:08 -0500

    Update mtgupload.inc.php

--- includes/mtgupload.inc.php
+++ includes/mtgupload.inc.php
@@ -1,15 +1,15 @@
 <?php
     include_once 'dbh_inc.php';
 
-    $mtgname = $_POST['MTG_Card_Name'];
-    $mtgset = $_POST['MTG_Card_Set'];
-    $mtgrare = $_POST['MTG_Card_Rarity'];
-    $mtglegend = $_POST['MTG_Legend'];
-    $mtgcost = $_POST['MTG_Cast'];
-    $mtgtotal = intval($_POST['MTG_total']);
-    $mtgcolorid = $_POST['Colorid'];
-    $mtgcardtype1 = $_POST['Cardtype1'];
-    $mtgcardtype2 = $_POST['Cardtype2'];
+    $mtgname = mysqli_real_escape_string($conn, $_POST['MTG_Card_Name']);
+    $mtgset = mysqli_real_escape_string($conn,$_POST['MTG_Card_Set']);
+    $mtgrare = mysqli_real_escape_string($conn, $_POST['MTG_Card_Rarity']);
+    $mtglegend = mysqli_real_escape_string($conn, $_POST['MTG_Legend']);
+    $mtgcost = mysqli_real_escape_string($conn, $_POST['MTG_Cast']);
+    $mtgtotal = mysqli_real_escape_string($conn, intval($_POST['MTG_total']));
+    $mtgcolorid = mysqli_real_escape_string($conn, $_POST['Colorid']);
+    $mtgcardtype1 = mysqli_real_escape_string($conn, $_POST['Cardtype1']);
+    $mtgcardtype2 = mysqli_real_escape_string($conn, $_POST['Cardtype2']);
 
     $sql = "INSERT INTO `capstone`.`mtg_card` (`MTGCardName`, `MTGCardSet`, `Rarity`, `Legendary`, `Cost`, `Total_Cost`, `ColorID`, `Card_Type1`, `Card_Type2`) VALUES('$mtgname', '$mtgset', '$mtgrare', '$mtglegend', '$mtgcost', '$mtgtotal', '$mtgcolorid', '$mtgcardtype1', '$mtgcardtype2');";
 
