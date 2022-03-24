325acdccbb3f99f032cfddabe7dcd92282541ff9
FXFY19 <99360332+FXFY19@users.noreply.github.com>
2022-03-24 16:44:23 -0400

    Add files via upload

---
+++ includes/deckstuff.php
@@ -0,0 +1,28 @@
+<?php
+
+    abstract class deck{
+
+        private $data = array();
+
+        function getData(){
+            return $this -> data;
+        }
+
+        abstract public function add_to_deck($Card_string);
+    }
+
+    class mtgdeck extends deck{
+        public function add_to_deck($mtg_string){
+            array_push($data, $mtg_string);
+        }
+    }
+
+    class cfvdeck extends deck{
+        public function add_to_deck($cfv_string){
+            array_push($data, $cfv_string);
+        }
+    }
+
+    
+
+?>
