e60700b5e72a223d85777f8954f6d877bc2b0b1f
FXFY19 <99360332+FXFY19@users.noreply.github.com>
2022-03-28 22:46:25 -0400

    Update header.php

--- header.php
+++ header.php
@@ -1,3 +1,9 @@
+<?php
+    session_start();
+?>
+
+
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -23,5 +29,7 @@
                 </ul>
             </div>
         </nav>
+
+        
     </body>
 </html>
