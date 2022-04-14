9ad233b64608336af779d4451cbc8a3791cf7efb
Felix Phommachanh <99360332+FXFY19@users.noreply.github.com>
2022-04-14 02:01:24 -0400

    Added Delete and Better Display

--- pkmcollection.php
+++ pkmcollection.php
@@ -3,6 +3,7 @@
 ?>
 
 <?php
+    include "includes/Cardpolymorph.php";
     include_once 'includes/dbh_inc.php';
 ?>
 
@@ -17,6 +18,15 @@
 
 </main>
 
+<h3 class = collection_display>
+    <form action = 'includes/all_card_delete.php' method = 'POST'>
+    <input type = 'submit' name = 'pkmdeleteItem' value = 'Delete All'>
+    </form>
+</h3>
+
+<br>
+
+
 <?php
     $sql = "SELECT * FROM capstone.pkm_card;";
     $result = mysqli_query($conn,$sql);
@@ -24,9 +34,28 @@
 
     if($result_check > 0){
         while ($row = mysqli_fetch_assoc($result)){
-            $carddisplay = $row['PKMName'] . "; " . $row['PKMSet'] . "; " . $row['PKMHP']. "; " . $row['PKMRarity']  . "; " . $row['PKMCardType'] . "; " . $row['EnergyType'] . "; " . $row['TrainerType'] . "; " . $row['Foil'] . "; " . $row['FoilType'];
+            $pkm_add = new PKM($row['PKMName'],$row['PKMRarity'], $row['PKMSet'],$row['EnergyType'], $row['PKMCardType'],$row['PKMHP'],$row['TrainerType'],$row['Foil'],$row['FoilType']);
+            $pkm_name = $pkm_add -> getCardname();
+            $pkm_set = $pkm_add -> getCardset();
+            $pkm_rare = $pkm_add -> getCardrarity();
+            $pkm_foil = $pkm_add -> getFoil();
+            $pkm_foil_type = $pkm_add -> getFoilType();
+            $pkm_type = $pkm_add -> getPKMCardType();
+            $pkm_trainer = $pkm_add -> getTrainer();
+            $pkm_energy = $pkm_add -> getEnergy();
+            $pkm_hp = $pkm_add -> getHP();
+
+
             echo "<h3 class= collection_display>
-                $carddisplay
+                PKM Card Name: $pkm_name <br>
+                      PKM Set: $pkm_set <br>
+                   PKM Rarity: $pkm_rare <br>
+                PKM Card Foil: $pkm_foil <br>
+                    Foil Type: $pkm_foil_type <br>
+                PKM Card Type: $pkm_type <br>
+                PKM Card Type: $pkm_trainer <br>
+                PKM Energy Type: $pkm_energy <br>
+                     HP Value: $pkm_hp <br>
                 </h3>";
             echo "<br>";
         }
