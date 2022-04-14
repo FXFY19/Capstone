de679011f04c04db0989e4625f46f90584d3f0d2
Felix Phommachanh <99360332+FXFY19@users.noreply.github.com>
2022-04-14 01:56:35 -0400

    Update TestforGlobal.php

--- TestforGlobal.php
+++ TestforGlobal.php
@@ -8,8 +8,15 @@ include "includes/Cardpolymorph.php";
 ?>
 
 <?php
-    $mtg_test = new MTG('counterspell','uncommon','commder legends','instant','none','No','UU','2','Blue(U)');
-    echo($mtg_test);
+    // $mtg_test = new MTG('counterspell','uncommon','commder legends','instant','none','No','UU','2','Blue(U)');
+    // $mtg_tester = $mtg_test -> getCardname();
+    // echo($mtg_tester);
+    // echo($mtg_test -> getCardname());
+    // $ygo_test = new YGO('Dark Magician','Rare','OG','Dark','Monster','Monster-normal','none', 7,2500,1000);
+    // echo($ygo_test -> getCardname());
+
+    $deck_test = new Deck("Test");
+    echo($deck_test-> get_deck_name());
 ?>
 
 <?php
