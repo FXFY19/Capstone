0b311dc663e88c7ec71ee6c2809081810537d79e
Felix Phommachanh <99360332+FXFY19@users.noreply.github.com>
2022-04-17 22:50:11 -0400

    Added Sort buttons

--- all_collection.php
+++ all_collection.php
@@ -19,90 +19,131 @@
 </main>
 
 <h3 class = collection_display>
+    Delete All Cards: <br>
+    *WARNING* Clicking this buttons will remove all the Cards from ALL Collections! You have been warned.
     <form action = 'includes/all_card_delete.php' method = 'POST'>
     <input type = 'submit' name = 'deleteItem' value = 'Delete All'>
     </form>
 </h3>
 <br>
 
+<h3 class = collection_display>
+    Sort Functions:
+    <br>
+    <form action = "all_collection.php" method = 'POST'>
+        <input type = 'submit' name = 'Default' value = 'Default Sort'>
+    </form>
+    <form action = "all_collection.php" method = 'POST'>
+        <input type = 'submit' name = 'All_Sort_Name' value = 'Sort by Card Name'>
+    </form>
+    <form action = "all_collection.php" method = 'POST'>
+        <input type = 'submit' name = 'All_Sort_Rarity' value = 'Sort by Rarity'>
+    </form>
+</h3>
+
+<br>
+
 
 <?php
-    $sql = "SELECT * FROM capstone.collection ORDER BY Card_Game, Card_Name Asc;";
-    $result = mysqli_query($conn,$sql);
-    $result_check = mysqli_num_rows($result);
 
-    if($result_check > 0){
-        while ($row = mysqli_fetch_assoc($result)){
-            $card_view = new Card($row['Card_Name'],$row['Card_Rarity'],$row['Card_Game_Set'],$row['Card_Game']);
-            $card_name = $card_view -> getCardname();
-            $card_rare = $card_view -> getCardrarity();
-            $card_set = $card_view -> getCardset();
-            $card_game = $card_view -> getGame();
-            echo "<h3 class= collection_display>
-                Card Game: $card_game <br>
-                Card Name: $card_name <br>
-                Card Set: $card_set <br>
-                Card Rarity: $card_rare <br>
-                </h3>";
-            echo "<br>";
+    //Default sort Button
+    if(isset($_POST['Default'])){
+        $sql = "SELECT * FROM capstone.collection ORDER BY Card_Game, Card_Name Asc;";
+        $result = mysqli_query($conn,$sql);
+        $result_check = mysqli_num_rows($result);
+
+        if($result_check > 0){
+            while ($row = mysqli_fetch_assoc($result)){
+                $card_view = new Card($row['Card_Name'],$row['Card_Rarity'],$row['Card_Game_Set'],$row['Card_Game']);
+                $card_name = $card_view -> getCardname();
+                $card_rare = $card_view -> getCardrarity();
+                $card_set = $card_view -> getCardset();
+                $card_game = $card_view -> getGame();
+                echo "<h3 class= collection_display>
+                    Card Game: $card_game <br>
+                    Card Name: $card_name <br>
+                    Card Set: $card_set <br>
+                    Card Rarity: $card_rare <br>
+                    </h3>";
+                echo "<br>";
+            }
         }
     }
+
+    //Sort by Name
+    else if(isset($_POST['All_Sort_Name'])){
+        $sql = "SELECT * FROM capstone.collection ORDER BY Card_Name Asc;";
+        $result = mysqli_query($conn,$sql);
+        $result_check = mysqli_num_rows($result);
+
+        if($result_check > 0){
+            while ($row = mysqli_fetch_assoc($result)){
+                $card_view = new Card($row['Card_Name'],$row['Card_Rarity'],$row['Card_Game_Set'],$row['Card_Game']);
+                $card_name = $card_view -> getCardname();
+                $card_rare = $card_view -> getCardrarity();
+                $card_set = $card_view -> getCardset();
+                $card_game = $card_view -> getGame();
+                echo "<h3 class= collection_display>
+                    Card Game: $card_game <br>
+                    Card Name: $card_name <br>
+                    Card Set: $card_set <br>
+                    Card Rarity: $card_rare <br>
+                    </h3>";
+                echo "<br>";
+            }
+        }
+    }
+
+    //Sort by Rarity
+    else if(isset($_POST['All_Sort_Rarity'])){
+        $sql = "SELECT * FROM capstone.collection ORDER BY Card_Rarity Asc;";
+        $result = mysqli_query($conn,$sql);
+        $result_check = mysqli_num_rows($result);
+
+        if($result_check > 0){
+            while ($row = mysqli_fetch_assoc($result)){
+                $card_view = new Card($row['Card_Name'],$row['Card_Rarity'],$row['Card_Game_Set'],$row['Card_Game']);
+                $card_name = $card_view -> getCardname();
+                $card_rare = $card_view -> getCardrarity();
+                $card_set = $card_view -> getCardset();
+                $card_game = $card_view -> getGame();
+                echo "<h3 class= collection_display>
+                    Card Game: $card_game <br>
+                    Card Name: $card_name <br>
+                    Card Set: $card_set <br>
+                    Card Rarity: $card_rare <br>
+                    </h3>";
+                echo "<br>";
+            }
+        }
+    }
+
+    //Default when on Site
+    else{
+        $sql = "SELECT * FROM capstone.collection ORDER BY Card_Game, Card_Name Asc;";
+        $result = mysqli_query($conn,$sql);
+        $result_check = mysqli_num_rows($result);
+
+        if($result_check > 0){
+            while ($row = mysqli_fetch_assoc($result)){
+                $card_view = new Card($row['Card_Name'],$row['Card_Rarity'],$row['Card_Game_Set'],$row['Card_Game']);
+                $card_name = $card_view -> getCardname();
+                $card_rare = $card_view -> getCardrarity();
+                $card_set = $card_view -> getCardset();
+                $card_game = $card_view -> getGame();
+                echo "<h3 class= collection_display>
+                    Card Game: $card_game <br>
+                    Card Name: $card_name <br>
+                    Card Set: $card_set <br>
+                    Card Rarity: $card_rare <br>
+                    </h3>";
+                echo "<br>";
+            }
+        }
+    }
+    
 ?>
 
 <?php
     require "footer.php";
 ?>
-
-
-
-
-<!-- Trying to create checkboxes to remove from database -->
-<!-- <script type="text/javascript" src="js/jquery-1.6.2.js"></script>
-<script type="text/javascript">
-	$(document).ready(function() {
-		$('#checkBoxAll').click(function() {
-			if ($(this).is(":checked"))
-				$('.chkCheckBoxId').prop('checked', true);
-			else
-				$('.chkCheckBoxId').prop('checked', false);
-		});
-	});
-</script>
-
-//<?php
-//require 'database.php';
-//if(isset($_POST['buttonDelete'])) {
-//	if(isset($_POST['Card_Name'])) {
-//		foreach ($_POST['Card_Name'] as $cardname) {
-//			$stmt = $conn->prepare('delete from card where Card_Name = :cardname');
-//			$stmt->bindValue('Card_Name', $cardname);
-//			$stmt->execute();
-//		}
-//	}
-//}
-//$stmt = $conn->prepare('select * from capstone.collection');
-//$stmt->execute();
-?>
-
-<form method="post" action="index.php">
-	<input type="submit" name="buttonDelete" value="Delete" onclick="return confirm('Are you sure?')" />
-	<table cellpadding="2" cellspacing="2" border="1">
-		<tr>
-			<th><input type="checkbox" id="checkBoxAll" /></th>
-			<th>Card Name</th>
-			<th>Card Rarity</th>
-			<th>Card Set</th>
-            <th>Card Game </th>
-		</tr>
-		//
-        <?php //while($cardname = $stmt->fetch(PDO::FETCH_OBJ)) { ?>
-		<tr>
-			<td><input type="checkbox" class="chkCheckBoxId"
-				value="//<?php //echo $account->username; ?>" name="username[]" /></td>
-			<td>//<?php //echo $account->username; ?></td>
-			<td>//<?php //echo $account->email; ?></td>
-			<td>//<?php //echo $account->age; ?></td>
-		</tr>
-		//<?php //} //?>
-	</table>
-</form> -->
